1ca8bf09e163b14cc868d7d81ad129558bc4caca
junalden <junalden@outlook.com>
2024-09-12 11:54:16 +0800

    For Submission Changed Youtube Vid

--- src/components/YoutubeCard.js
+++ src/components/YoutubeCard.js
@@ -22,7 +22,7 @@ export const YoutubeCard = () => {
           {/* 16:9 aspect ratio */}
           <iframe
             className="absolute top-0 left-0 w-full h-full"
-            src="https://www.youtube.com/embed/-txYrD3qQWk?controls=1&showinfo=0&rel=0&modestbranding=1"
+            src="https://www.youtube.com/embed/keRmyh5Zc8w?controls=1&showinfo=0&rel=0&modestbranding=1"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
